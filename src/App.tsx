3ed0ce0e1fac252c9420d1e4972b6655575634aa
vivek1111a <112795819+vivek1111a@users.noreply.github.com>
2025-02-10 03:12:52 +0530

    Added shad cn and initialized project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
