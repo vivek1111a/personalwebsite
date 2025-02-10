6aa17ceb467a5f42e2fb25c96408135471c87f22
vivek1111a <112795819+vivek1111a@users.noreply.github.com>
2025-02-10 17:12:56 +0530

    Add footer component and style

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,25 @@ import Navbar from "../boilerplate/Navbar";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Blog from "../pages/Blog";
+import Footer from "../boilerplate/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="*" element={<Pagenotfound />} />
-        </Routes>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="*" element={<Pagenotfound />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
